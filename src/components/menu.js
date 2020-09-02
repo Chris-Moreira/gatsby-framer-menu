da4debbe20fd16e3ceed54af33d3a66da9800886
Chris Moreira <moreira22@hotmail.co.uk>
2020-09-02 12:15:51 +0300

    Finished animation

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,42 +1,84 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { Image } from "../components/gatsby-images/image"
 
+//Gatsby image
+import { motion, AnimatePresence } from "framer-motion"
+
+//Data
 import data from "../data/products.json"
 
+//Icons
 import { Close } from "../icons/icons"
+
+//Transition
+const transition = { duration: 0.8, ease: [0.6, -0.05, 0.01, 0.9] }
+
+//Variants
+
+const parent = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 1,
+    },
+  },
+}
+
+const titleSlideUp = {
+  initial: { y: 200 },
+  animate: { y: 0 },
+}
+
+const maskAnimation = {
+  initial: { width: "100%" },
+  animate: { width: 0 },
+}
 
 const Menu = ({ menuState, setMenuState }) => {
   return (
-    <>
+    <AnimatePresence>
       {menuState && (
-        <div className="products">
-          <div className="menu-title">Products</div>
-          <div onClick={() => setMenuState(false)} className="close">
-            <Close />
-          </div>
-          <div className="menu">
-            <div className="container">
-              <div className="menu-inner">
-                <ul>
-                  {data.map(list => (
-                    <List
-                      key={list.id}
-                      id={list.id}
-                      title={list.title}
-                      src={list.src}
-                      leftLineFlex={list.leftLineFlex}
-                      rightLineFlex={list.rightLineFlex}
-                      thumbnailPosition={list.thumbnailPosition}
-                    />
-                  ))}
-                </ul>
+        <>
+          <motion.div
+            initial={{ visibility: "hidden" }}
+            animate={{ visibility: "visible", transition: { delay: 1 } }}
+            exit={{ visibility: "hidden", transition: { delay: 1 } }}
+            className="products"
+          >
+            <div className="menu-title">Products</div>
+            <div onClick={() => setMenuState(false)} className="close">
+              <Close />
+            </div>
+            <div className="menu">
+              <div className="container">
+                <div className="menu-inner">
+                  <motion.ul
+                    variants={parent}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                  >
+                    {data.map(list => (
+                      <List
+                        key={list.id}
+                        id={list.id}
+                        title={list.title}
+                        src={list.src}
+                        leftLineFlex={list.leftLineFlex}
+                        rightLineFlex={list.rightLineFlex}
+                        thumbnailPosition={list.thumbnailPosition}
+                      />
+                    ))}
+                  </motion.ul>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </motion.div>
+          <Panels />
+        </>
       )}
-    </>
+    </AnimatePresence>
   )
 }
 
@@ -53,26 +95,74 @@ const List = ({
       <Link to={`/product/${id}`}>
         <div className="wrapper">
           <div className={`line left flex-${leftLineFlex}`}>
-            {/* <div className="mask"></div> */}
+            <motion.div
+              variants={maskAnimation}
+              transition={{ ...transition, duration: 1 }}
+              className="mask"
+            ></motion.div>
           </div>
           <div className="title">
             <h2>
-              <div className="text">{title}</div>
+              <motion.div
+                variants={titleSlideUp}
+                transition={transition}
+                className="text"
+              >
+                {title}
+              </motion.div>
             </h2>
           </div>
           <div className="thumbnail" style={{ left: thumbnailPosition }}>
             <Image src={src} />
-            {/* <div className="mask"></div> */}
+            <motion.div
+              variants={maskAnimation}
+              transition={{ ...transition, duration: 1 }}
+              className="mask"
+            ></motion.div>
           </div>
           <div className="floating-image">
             <Image src={src} />
           </div>
           <div className={`line right flex-${rightLineFlex}`}>
-            {/* <div className="mask right"></div> */}
+            <motion.div
+              variants={maskAnimation}
+              transition={{ ...transition, duration: 1 }}
+              className="mask right"
+            ></motion.div>
           </div>
         </div>
       </Link>
     </li>
+  )
+}
+
+const Panels = () => {
+  const [panelComplete, setPanelComplete] = useState(false)
+  return (
+    <>
+      <motion.div
+        style={{ background: panelComplete ? "#e7e7de" : "#e7dee7" }}
+        initial={{ height: 0 }}
+        animate={{ height: [0, window.innerHeight, 0], bottom: [null, 0, 0] }}
+        exit={{ height: [0, window.innerHeight, 0], top: [null, 0, 0] }}
+        transition={{ ...transition, duration: 2, times: [0, 0.5, 1] }}
+        className="left-panel-background"
+      ></motion.div>
+      <motion.div
+        style={{ background: panelComplete ? "#e7e7de" : "#e7dee7" }}
+        initial={{ height: 0 }}
+        animate={{
+          height: [0, window.innerHeight, 0],
+          bottom: [0, 0, window.innerHeight],
+        }}
+        exit={{ height: [0, window.innerHeight, 0], bottom: [null, 0, 0] }}
+        transition={{ ...transition, duration: 2, times: [0, 0.5, 1] }}
+        className="right-panel-background"
+        onAnimationComplete={() => {
+          setPanelComplete(!panelComplete)
+        }}
+      ></motion.div>
+    </>
   )
 }
 
